41fa18e951122c1224b4bf503fd569f8806af01d
danpaul <pt2323@gmail.com>
2016-01-02 15:26:16 -0500

    cleanup

--- test/index.js
+++ test/index.js
@@ -3,13 +3,13 @@ var async = require('async');
 module.exports = function(app, callbackIn){
     app.testData = {};
     async.series([
-        // function(callback){ require('./model')(app, callback); },
-        // function(callback){ require('./feed')(app, callback); },
-        // function(callback){ require('./user')(app, callback); },
-        // function(callback){ require('./routes')(app, callback); },
-        // function(callback){
-        //     delete app.testData;
-        //     callback();
-        // }
+        function(callback){ require('./model')(app, callback); },
+        function(callback){ require('./feed')(app, callback); },
+        function(callback){ require('./user')(app, callback); },
+        function(callback){ require('./routes')(app, callback); },
+        function(callback){
+            delete app.testData;
+            callback();
+        }
     ], callbackIn);
 }
